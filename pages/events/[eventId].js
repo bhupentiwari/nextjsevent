1abeced29a36b7238f40e22041dea6913588dc40
Bhupendra Tiwari <bhupentiwari@live.com>
2022-06-23 08:31:07 +0530

    Completed till all events chapter 79

--- pages/events/[eventId].js
+++ pages/events/[eventId].js
@@ -1,10 +1,31 @@
+import { useRouter } from "next/router";
+import { Fragment } from "react";
+import { getEventById } from "../../dummy-data";
+import EventSummary from "../../components/event-detail/event-summary";
+import EventLogistics from "../../components/event-detail//event-logistics";
+import EventContent from "../../components/event-detail/event-content";
 
 export default function AllEventsDetailsPage() {
-    return (
-      <div>
-      <h1>
-      All Events Details Page      </h1>
-      </div>
-    )
+  const router = useRouter();
+  const eventId = router.query.eventId;
+
+  const event = getEventById(eventId);
+  console.log(event);
+  if (!event) {
+    return <p>No Event found !!!</p>;
   }
-  
+  return (
+    <Fragment>
+      <EventSummary title={event.title} />
+      <EventLogistics
+        date={event.date}
+        address={event.location}
+        image={event.image}
+        imageAlt={event.title}
+      />
+      <EventContent>
+        <p>{event.description}</p>
+      </EventContent>
+    </Fragment>
+  );
+}
