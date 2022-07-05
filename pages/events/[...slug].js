f1af096df98209ff2aec42abc64e3d49c11d5a83
Bhupendra Tiwari <bhupentiwari@live.com>
2022-07-05 13:07:19 +0530

    Section 6 Page Pre-rendering and data fetching

--- pages/events/[...slug].js
+++ pages/events/[...slug].js
@@ -1,37 +1,70 @@
 import {useRouter} from 'next/router';
 import EventList from '../../components/events/event-list';
-import { getFilteredEvents } from '../../dummy-data';
+import { getFilteredEvents } from '../../helpers/api-util';
 
-function FilteredEventsPage(){
+function FilteredEventsPage(props){
 
-    const router = useRouter();
-    const filterValue = router.query.slug;
-    console.log(filterValue);
+   // const router = useRouter();
+    //const filterValue = router.query.slug;
+   // console.log(filterValue);
 
-    if(!filterValue){
-        return <p>No filter value</p>
-    }
+    // if(!filterValue){
+    //     return <p className='center'>Loading ....</p>
+    // }
 
-    const month = filterValue[1];
-    const year = filterValue[0];
+    // const month = filterValue[1];
+    // const year = filterValue[0];
 
-    const numMonth = +month;
-    const numYear = +year;
+    // const numMonth = +month;
+    // const numYear = +year;
 
-    const data = getFilteredEvents({
-        'year': numYear,
-        'month' : numMonth
-    });
-    debugger
-    if(data.length <=0){
+    // const data = getFilteredEvents({
+    //     'year': numYear,
+    //     'month' : numMonth
+    // });
+    //debugger
+    if(props.data.length == 0){
         return <p>No data found</p>
     }
+    else
     return (
         <div>
             <h1>Filtered Events</h1>
-            <EventList items = {data} />
+            <EventList items = {props.data} />
         </div>
     )
 }
 
 export default FilteredEventsPage
+
+export async function getServerSideProps(context) {
+
+    const filterValue = context.params.slug;
+  
+     if(!filterValue){
+         return {
+            props : {hasError : true}
+     }
+    }
+     const month = filterValue[1];
+     const year = filterValue[0];
+ 
+     const numMonth = +month;
+     const numYear = +year;
+    
+     const data = await getFilteredEvents({
+        'year': numYear,
+        'month' : numMonth
+    });
+
+    return {
+        props : {
+            data : data,
+            year : {
+                month : numMonth,
+                year : numYear
+            }
+        }
+    }
+
+}
--- pages/events/[eventId].js
+++ pages/events/[eventId].js
@@ -1,18 +1,17 @@
-import { useRouter } from "next/router";
 import { Fragment } from "react";
-import { getEventById } from "../../dummy-data";
+import {
+  getEventById,
+  getFeaturedEventsFromHelper,
+} from "../../helpers/api-util";
 import EventSummary from "../../components/event-detail/event-summary";
 import EventLogistics from "../../components/event-detail//event-logistics";
 import EventContent from "../../components/event-detail/event-content";
 
-export default function AllEventsDetailsPage() {
-  const router = useRouter();
-  const eventId = router.query.eventId;
+export default function AllEventsDetailsPage(props) {
+  const { event } = props;
 
-  const event = getEventById(eventId);
-  console.log(event);
   if (!event) {
-    return <p>No Event found !!!</p>;
+    return <div className="center">No Event found !!!</div>;
   }
   return (
     <Fragment>
@@ -28,4 +27,27 @@ export default function AllEventsDetailsPage() {
       </EventContent>
     </Fragment>
   );
+}
+export async function getStaticProps(context) {
+  const eventId = context.params.eventId;
+  const events = await getEventById(eventId);
+
+  return {
+    props: {
+      event: events,
+    },
+    revalidate: 30,
+  };
+}
+
+export async function getStaticPaths() {
+  const allEvents = await getFeaturedEventsFromHelper();
+  const formatPath = allEvents.map((event) => ({
+    params: { eventId: event.id },
+  }));
+  return {
+    paths: formatPath,
+    //fallback: true,
+    fallback: 'blocking',
+  };
 }
